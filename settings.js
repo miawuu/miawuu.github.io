a006304d04f7e79f73ed3101482bef6b129dd325
miawuu <69891692+miawuu@users.noreply.github.com>
2020-09-01 20:46:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,14 @@
 var defaultSettings = {
 
-    align: 'c',
-    quality: 'low',
-    scale: 'default',
-    wmode: 'window',
+    align: 'b',         // possible values: c / t / l / r / b
+    // c means center, t means top, l means left, right means right, b means bottom
+
+    quality: 'high',    // possible values: low / medium / high / autolow / autohigh
+
+    scale: 'default',   // possible values: default / noborder / exactfit / noscale
+
+    wmode: 'window',    // possible values: window / direct / opaque / transparent / gpu
+
     background: '#6A7495'
 
 }
